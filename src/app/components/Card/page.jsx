38df698f34bac45475350a7ff4cfacbe55102aa9
Lakshay Rajput <rajputlakshay553@gmail.com>
2023-11-10 09:56:32 +0530

    Edit Blog page

--- src/app/components/Card/page.jsx
+++ src/app/components/Card/page.jsx
@@ -1,56 +1,36 @@
 
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image';
 
 
-const Card = () => {
+const Card = ({ image, link, title, context }) => {
+
   return (
-    <div className='flex flex-col mr-20'>
-          <div className=' border-2 border-solid  border-#e0e0e0 w-80 p-4 mb-5 ml-10 rounded-lg'>
-     
-      <img src="/image1.jpg" alt="omg" className=' border border-solid  border-#e0e0e0 rounded-lg '/>
-      <h1 className='text-xl font-bold pt-2'>This is a title of a post</h1>
-      <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quibusdam assumenda maxime accusantium?</p>
-      <Link href="/Pages/Blog/Rahi"> <button className='py-2 px-3 bg-blue-700 font-bold text-white mt-5 rounded-lg'>
-      Read Me</button></Link>
-      </div>
+    <div className=' border-2 border-solid  border-#e0e0e0 rounded-lg flex flex-col p-2 h-72 w-56 gap-1'>
 
-      <div className=' border-2 border-solid  border-#e0e0e0 w-80 p-4 mb-5 ml-10 rounded-lg'>
-      
-      <img src="/image1.jpg" alt="omg" className=' border border-solid  border-#e0e0e0 rounded-lg '/>
-      <h1 className='text-xl font-bold pt-2'>This is a title of a post</h1>
-      <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quibusdam assumenda maxime accusantium?</p>
-      <Link href="/Pages/Blog/Lakshay "><button className='py-2 px-3 bg-blue-700 font-bold text-white mt-5 rounded-lg'>
-      Read Me</button></Link>
-      </div>
+      <Image
+        src={`/${image}`}
+        alt="img"
+        layout="responsive"
+        width={208}
+        height={208}
+        className='rounded-lg'
+      />
 
-      <div className=' border-2 border-solid  border-#e0e0e0 w-80 p-4 mb-5 ml-10 rounded-lg'>
-      
-      <img src="/image1.jpg" alt="omg" className=' border border-solid  border-#e0e0e0 rounded-lg '/>
-      <h1 className='text-xl font-bold pt-2'>This is a title of a post</h1>
-      <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quibusdam assumenda maxime accusantium?</p>
-      <Link href="/Pages/Blog/Disha"><button className='py-2 px-3 bg-blue-700 font-bold text-white mt-5 rounded-lg'>
-      Read Me</button></Link>
+      <div className='pl-1'>
+        <h1 className=' text-base font-semibold'>{title}</h1>
+        <p className=' text-xs font-light'>{context}</p>
+
       </div>
-      <div className=' border-2 border-solid  border-#e0e0e0 w-80 p-4 mb-5 ml-10 rounded-lg'>
-      
-      <img src="/image1.jpg" alt="omg" className=' border border-solid  border-#e0e0e0 rounded-lg '/>
-      <h1 className='text-xl font-bold pt-2'>This is a title of a post</h1>
-      <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quibusdam assumenda maxime accusantium?</p>
-      <button className='py-2 px-3 bg-blue-700 font-bold text-white mt-5 rounded-lg'>
-      Read Me</button>
-      </div>
-      <div className=' border-2 border-solid  border-#e0e0e0 w-80 p-4 mb-5 ml-10 rounded-lg'>
-      
-      <img src="/image1.jpg" alt="omg" className=' border border-solid  border-#e0e0e0 rounded-lg '/>
-      <h1 className='text-xl font-bold pt-2'>This is a title of a post</h1>
-      <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quibusdam assumenda maxime accusantium?</p>
-      <button className='py-2 px-3 bg-blue-700 font-bold text-white mt-5 rounded-lg'>
-      Read Me</button>
-      </div>
+      <Link
+        className='p-2 w-fit text-xs bg-blue-700 font-medium text-white rounded-lg'
+        href={`/Blog/${link}`}>
+        Read Me
+      </Link>
+
     </div>
-   
-  )
+  );
 }
 
 export default Card
