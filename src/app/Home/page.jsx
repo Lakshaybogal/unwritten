df3d61fd1b2ed76f7066dde434954b95ee3055d5
Lakshay Rajput <rajputlakshay553@gmail.com>
2024-01-02 12:40:21 +0530

    Update home page

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,48 +1,94 @@
 "use client"
-import React from "react";
 import Carousel from "@/components/HomePage/Carousel";
-import Card from "@/components/Card/page";
-import '@/utili/pages.css'
-
+import '@/utili/home.css';
+import Image from 'next/image';
+import Animation from '../components/Animations/HeroSection';
+import {LuBrainCircuit} from "react-icons/lu";
 const Home = () => {
+  const perks = [
+    {
+      icon :<LuBrainCircuit/>,
+      title: "Learning",
+      img: "/image2.jpg",
+      desc: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+    },
+    {
+      icon :<LuBrainCircuit/>,
+      title: "Learning",
+      img: "/image2.jpg",
+      desc: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+    },
+    {
+      icon :<LuBrainCircuit/>,
+      title: "Learning",
+      img: "/image2.jpg",
+      desc: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+    }
+  ]
   return (
     <>
-      <div className="flex flex-row flex-wrap pt-10 justify-between">
-        <div className="p-10">
-          <h1 className=" hero_heading text-5xl font-bold ">
+      <div className="flex flex-col-reverse lg:flex-row flex-wrap pt-10 justify-between items-center">
+        <div className="flex flex-col">
+          <h1 className=" blur_bg hero_heading text-4xl lg:text-7xl font-bold ">
             Your daily dose of
             inspiration and insight
           </h1>
-          <p className="font-normal pt-5">
+          <p className="text-xl lg:text-2xl font-normal pt-5">
             Experience the power of knowledge
           </p>
-          <button className=" mt-5 bg-blue-700 text-white font-semibold py-2 px-3 rounded-lg">
+          <button className=" text-xl lg:text-2xl mt-3 w-[50%] lg:w-[30%] bg-blue-700 text-white font-semibold py-2 px-2 rounded-lg">
             Explore Now
           </button>
-          <ul className="flex flex-row font-light pt-7">
-            <li className="mr-4 flex flex-col"> <span className="text-blue-700 font-bold mr-6 ">+394</span>Subscribe</li>
-            <li className="mr-4 flex flex-col"> <span className="text-blue-700 font-bold mr-6 ">+394</span>Readers</li>
-            <li className="mr-4 flex flex-col"> <span className="text-blue-700 font-bold mr-6 ">+394</span>Writers</li>
+          <ul className="flex flex-row font-light pt-7 gap-2  ">
+            <li className="text-xl lg:text-2xl flex flex-col"> <span className="text-blue-700 font-bold">+394</span>Subscribe</li>
+            <li className="text-xl lg:text-2xl flex flex-col"> <span className="text-blue-700 font-bold">+394</span>Readers</li>
+            <li className="text-xl lg:text-2xl flex flex-col"> <span className="text-blue-700 font-bold">+394</span>Writers</li>
           </ul>
         </div>
-
-        <img src="/image2.jpg" className="  w-[50%]" />
-
+        <div className=" lg:w-[45%]">
+          <Animation />
+        </div>
       </div>
-
-      {/* <div className="flex flex-row justify-between mt-10 bottom-4 m-1">
-        <button className="border border-gray-400 rounded-lg px-3 py-1 hover:bg-gray-400">All</button>
-        <button className="border border-gray-400 rounded-lg px-3 py-1  hover:bg-gray-400">DSA</button>
-        <button className="border border-gray-400 rounded-lg px-3 py-1  hover:bg-gray-400">React</button>
-        <button className="border border-gray-400 rounded-lg px-3 py-1  hover:bg-gray-400">Python</button>
-        <button className="border border-gray-400 rounded-lg px-3 py-1  hover:bg-gray-400">Next</button>
-        <button className="border border-gray-400 rounded-lg px-3 py-1  hover:bg-gray-400">HTML</button>
-        <button className="border border-gray-400 rounded-lg px-3 py-1  hover:bg-gray-400">CSS</button>
-        <button className="border border-gray-400 rounded-lg px-3 py-1  hover:bg-gray-400">Angular</button>
-
-      </div> */}
+      <section className="blur_bg flex flex-col justify-center items-center py-5">
+        <h1 className="text-4xl lg:text-5xl font-bold text-blue-700">2X Your knowledge</h1>
+        <div className="flex flex-col lg:flex-row lg:p-5 ">
+          {
+            perks.map((perk) => {
+              return (
+                <div
+                  className="p-3"
+                  key={perk.img}>
+                  <div className="flex items-center gap-1 text-3xl font-semibold p-2">
+                    <span>
+                      {perk.icon}
+                    </span>
+                  
+                  <h2 >
+                    {
+                      perk.title
+                    }
+                  </h2>
+                  </div>
+                  <div className="p-2">
+                  <Image src={perk.img} 
+                  layout = 'responsive'
+                  style = {{
+                    borderRadius : "10px",
+                  }}
+                  width={100} height={100} 
+                  alt='' />
+                  </div>
+                  <p className=" text-lg font-medium p-2">
+                    {perk.desc}
+                  </p>
+                </div>
+                )
+            })
+          }
+        </div>
+      </section>
       <Carousel />
-      {/* <Card/> */}
+
     </>
   );
 };
